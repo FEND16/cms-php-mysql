d36206b0c2823e8040c9b239b39347c9a1e4c8a2
Jesper Orb <jesper.orb@zocom.se>
2017-04-18 08:42:16 +0200

    Added OOP Slides

---
+++ code/04_Elephant.php
@@ -0,0 +1,35 @@
+<?php
+
+class Elephant
+{
+
+  private $name;
+  public static $age = 50;
+
+  public function __construct($name = 'Hathi')
+  {
+    $this->name = $name;
+  }
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public static function sayStuff()
+  {
+   echo Self::$age;
+  }
+}
+
+$hathi = new Elephant;
+$hathi->setName('Dumbo');
+$hathiName = (new Elephant)->sayStuff();
+echo $hathiName;
+
+$hathi->getName(); 
+Elephant::sayStuff();
